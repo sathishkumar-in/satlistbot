7df6eb70e5ad282328008a004163dfed6e970839
Sathish Kumar S <sathishkumarindian18@gmail.com>
2017-12-03 19:30:20 +0530

    modified a file

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-print("Hello World")
-print("Stupid, Cupid")
-a=5
-l=[1,2,4]
+import time
+import telepot
+import sys
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    chat_id=msg['chat']['id']
+    command=msg['text']
+
+    print("Got command",command)
+    if command=='/help':
+        bot.sendMessage(chat_id,"Type /list to list all tasks")
+    if command.
+    elif command=='/list':
+        bot.sendMessage(chat_id,"Listing all items")
+    elif command=='/about':
+        bot.sendMessage(chat_id,"Created by Sathish")
+
+TOKEN=sys.argv[1]
+bot=telepot.Bot(TOKEN)
+
+MessageLoop(bot,handle).run_as_thread()
+print('I am listening ...')
+
+while 1:
+    time.sleep(10)
